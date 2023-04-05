636f88b473949a87f50bc8b24108d369e81d69f9
Shaheen Nijamudheen <67351194+Snijamudheen@users.noreply.github.com>
2023-04-05 03:23:49 -0500

    Update and rename college_registration to college_registration.cpp

--- college_registration.cpp
+++ college_registration.cpp
@@ -1,4 +1,6 @@
-/*The college management system helps to keep records of students' tuition fees. The code asks if the student is in-state or out-of-state. For the purpose of calculating the tuition fees, it also asks for the health insurance plan. It then displays the details upon request. C++ was chosen as the programming language for a procedural programming solution.*/
+/*The college management system helps to keep records of students' tuition fees. The code asks if the student is in-state or out-of-state. 
+For the purpose of calculating the tuition fees, it also asks for the health insurance plan. It then displays the details upon request. 
+C++ was chosen as the programming language for a procedural programming solution.*/
 
 #include <iostream>
 #include <fstream>

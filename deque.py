7f74f1054efe17a98be3bf04e05896a3b9e18b3c
Shaheen Nijamudheen <67351194+Snijamudheen@users.noreply.github.com>
2023-04-15 06:14:09 -0500

    Update deque.py

--- deque.py
+++ deque.py
@@ -1,4 +1,4 @@
-/*A python program that uses the conecpt of queue for storing and deleting data. Queue is a linear data structure that stores items in a First-In/First Out(FIFO) manner.*/
+#A python program that uses the conecpt of queue for storing and deleting data. Queue is a linear data structure that stores items in a First-In/First Out(FIFO) manner.
 
 def insertFront(myDeque,element):
     myDeque.insert(0,element)

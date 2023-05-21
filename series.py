6cf30dc5b61c91d3579d27b6dba834fb137c9dac
Shaheen Nijamudheen <67351194+Snijamudheen@users.noreply.github.com>
2023-05-20 21:49:20 -0400

    Update and rename series to series.py

--- series.py
+++ series.py
@@ -1,4 +1,4 @@
-/*A python program that calculates the sum of the series: 1 + 1/2 + 1/3 + 1/4...+ 1/n*/
+#A python program that calculates the sum of the series: 1 + 1/2 + 1/3 + 1/4...+ 1/n
 
 n = int(input("Enter an integer: "))
 sum = 0

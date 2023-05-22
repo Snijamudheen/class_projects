6cdce2192da66b9f230d7c5a20809cf4d8cad926
Shaheen Nijamudheen <67351194+Snijamudheen@users.noreply.github.com>
2023-05-21 22:33:18 -0400

    Update and rename search to search.cpp

--- search.cpp
+++ search.cpp
@@ -1,4 +1,6 @@
-/*The binary search algorithm divides the interval of search in half repeatedly to determine the location of the value it is looking for within a sorted array. The interval should be narrowed to the lower half if the value of the search key is less than the middle element. It should otherwise be narrowed to the upper half. Binary search is more efficient than linear search. Compared to linear searches, binary searches are more complex in nature.*/
+/*The binary search algorithm divides the interval of search in half repeatedly to determine the location of the value it is looking for within a sorted array. \
+The interval should be narrowed to the lower half if the value of the search key is less than the middle element. It should otherwise be narrowed to the upper half.
+Binary search is more efficient than linear search. Compared to linear searches, binary searches are more complex in nature.*/
 
 #include <iostream>
 using namespace std;
@@ -17,46 +19,40 @@ int main()
 
     results = binaryS(idnums, SIZE, empID);
 
-    if(results == -1){
-    
+    if(results == -1)
+    {
         cout << "The ID does not exist." << endl;
-        
-    }else{
-
+    }
+    else
+    {
         cout << "The ID is found at position " << results + 1 << endl;
-
     }
 
     return 0;
 }
 
-int binaryS (const int arr[], int size, int value){
-
+int binaryS (const int arr[], int size, int value)
+{
     int first = 0;
     int last = size - 1;
     int position = -1;
     bool found = false;
     int middle;
 
-    while(first <= last && !found){
-
+    while(first <= last && !found)
+    {
         middle = (first + last) / 2; 
         
-        if(arr[middle] == value){
-        
+        if(arr[middle] == value)
+        {    
             found = true;
             position = middle;
-            
         }
         else if(arr[middle] > value)
-        
-            last = middle - 1;
-            
-        else
-        
+            last = middle - 1;       
+        else    
             first = middle + 1;
     }
     
     return position;
-
 }

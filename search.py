0f3cccb59b1a18cf54589cd881bb80c85ca87fc8
Shaheen Nijamudheen <67351194+Snijamudheen@users.noreply.github.com>
2023-06-06 14:18:43 -0500

    Update and rename search_py to search.py

--- search.py
+++ search.py
@@ -1,9 +1,8 @@
-/*This is a small python program checks whether an element is present in the list and displays its index position.*/
+#This is a small python program checks whether an element is present in the list and displays its index position.
 
 def check(list1,n):
     for num in list1(0,n):
         
-
 if num == n:
     print("the number ",n," is present in the list")
 else:
@@ -14,4 +13,3 @@ n = int(input("enter the number you want to check from list1: "))
 list1=[1,2,3,4,5]
 
 check(list1,n)
-        

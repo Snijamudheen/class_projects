9ab2175321c3ac9bfa2b115b6e02284968357801
Shaheen Nijamudheen <67351194+Snijamudheen@users.noreply.github.com>
2023-06-07 19:11:29 -0500

    Update and rename selection_sort to selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -3,52 +3,53 @@
 #include <iostream>
 using namespace std;
 
-void selectionsort(int[],int);
+void selectionsort(int[], int);
 void swap(int &, int &);
 
 int main()
 {
     const int SIZE = 6;
-    int values[SIZE]={3,2,5,4,1,6};
+    int values[SIZE] = {3,2,5,4,1,6};
 
-    cout<<"The unsorted array is: "<<endl;
+    cout << "The unsorted array is: " << endl;
     for(auto element:values)
-        cout<<element<<" ";
-    cout<<endl;
+        cout << element << " ";
+    cout << endl;
 
     selectionsort(values, SIZE);
 
-    cout<<"The unsorted array is: "<<endl;
+    cout << "The unsorted array is: " << endl;
     for(auto element:values)
-        cout<<element<<" ";
-    cout<<endl;
-
+        cout << element << " ";
+    cout << endl;
 }
 
-void selectionsort(int array[],int size){
-
+void selectionsort(int array[], int size)
+{
     int minval;
     int minindex;
 
-    for(int start = 0;start < (size-1);start++){
-
+    for(int start = 0; start < (size - 1); start++)
+    {
         minindex = start;
         minval = array[start];
 
-        for(int index = start+1 ;index< size;index++){
-
-            if(array[index]> minval){
+        for(int index = start + 1 ; index < size; index++)
+        {
+            if(array[index] > minval)
+            {
                 minval = array[index];
                 minindex = index;
             }
         }
-        swap(array[minindex],array[start]);
+        swap(array[minindex], array[start]);
     }
 }
- void swap(int &a,int &b){
+
+void swap(int &a, int &b)
+{
     int temp;
     temp = a;
-    a= b;
-    b=temp;
-
- }
+    a = b;
+    b = temp;
+}

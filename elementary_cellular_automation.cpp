3ff8623d718c44ec05612f3b7f286952b4c8a24a
Shaheen Nijamudheen <67351194+Snijamudheen@users.noreply.github.com>
2023-12-27 03:13:55 -0600

    Update elementary_cellular_automation.cpp

--- elementary_cellular_automation.cpp
+++ elementary_cellular_automation.cpp
@@ -3,3 +3,35 @@ The rules of evolution are then encoded with eight bits indicating the outcome o
 The purpose of this task is to create a subroutine, program or function that allows to create and visualize the evolution of any of the 256 possible elementary cellular automaton of arbitrary space length and for any given initial state. You can demonstrate your solution with any automaton of your choice.
 The space state should wrap: this means that the left-most cell should be considered as the right neighbor of the right-most cell, and reciprocally.
 This task is basically a generalization of one-dimensional cellular automaton.*/
+
+#include <bitset>
+#include <stdio.h>
+
+#define SIZE	           80
+#define RULE               30
+#define RULE_TEST(x)       (RULE & 1 << (7 & (x)))
+
+void evolve(std::bitset<SIZE> &s) {
+	int i;
+	std::bitset<SIZE> t(0);
+	t[SIZE-1] = RULE_TEST( s[0] << 2 | s[SIZE-1] << 1 | s[SIZE-2] );
+	t[     0] = RULE_TEST( s[1] << 2 | s[     0] << 1 | s[SIZE-1] );
+	for (i = 1; i < SIZE-1; i++)
+	t[i] = RULE_TEST( s[i+1] << 2 | s[i] << 1 | s[i-1] );
+	for (i = 0; i < SIZE; i++) s[i] = t[i];
+}
+void show(std::bitset<SIZE> s) {
+	int i;
+	for (i = SIZE; --i; ) printf("%c", s[i] ? '#' : ' ');
+	printf("\n");
+}
+int main() {
+	int i;
+	std::bitset<SIZE> state(1);
+	state <<= SIZE / 2;
+	for (i=0; i<10; i++) {
+		show(state);
+		evolve(state);
+	}
+	return 0;
+}

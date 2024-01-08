d8e924474636eb44eb238137a2d08429ca3903fa
Shaheen Nijamudheen <67351194+Snijamudheen@users.noreply.github.com>
2024-01-07 19:26:59 -0600

    Update elementary_cellular_automation.cpp

--- elementary_cellular_automation.cpp
+++ elementary_cellular_automation.cpp
@@ -11,25 +11,38 @@ This task is basically a generalization of one-dimensional cellular automaton.*/
 #define RULE               30
 #define RULE_TEST(x)       (RULE & 1 << (7 & (x)))
 
-void evolve(std::bitset<SIZE> &s) {
+void evolve(std::bitset<SIZE> &s) 
+{
 	int i;
 	std::bitset<SIZE> t(0);
 	t[SIZE-1] = RULE_TEST( s[0] << 2 | s[SIZE-1] << 1 | s[SIZE-2] );
 	t[     0] = RULE_TEST( s[1] << 2 | s[     0] << 1 | s[SIZE-1] );
+	
 	for (i = 1; i < SIZE-1; i++)
-	t[i] = RULE_TEST( s[i+1] << 2 | s[i] << 1 | s[i-1] );
-	for (i = 0; i < SIZE; i++) s[i] = t[i];
+		t[i] = RULE_TEST( s[i+1] << 2 | s[i] << 1 | s[i-1] );
+	
+	for (i = 0; i < SIZE; i++) 
+		s[i] = t[i];
 }
-void show(std::bitset<SIZE> s) {
+
+void show(std::bitset<SIZE> s) 
+{
 	int i;
-	for (i = SIZE; --i; ) printf("%c", s[i] ? '#' : ' ');
+	
+	for (i = SIZE; --i; ) 
+		printf("%c", s[i] ? '#' : ' ');
+	
 	printf("\n");
 }
-int main() {
+
+int main() 
+{
 	int i;
 	std::bitset<SIZE> state(1);
 	state <<= SIZE / 2;
-	for (i=0; i<10; i++) {
+	
+	for (i = 0; i < 10; i++) 
+	{
 		show(state);
 		evolve(state);
 	}
